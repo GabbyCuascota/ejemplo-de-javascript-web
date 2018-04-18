1c2a9295261797941981f845f7b0d94b23f4357d
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-04-18 12:32:24 -0500

    3

--- Variables.js
+++ Variables.js
@@ -1,1 +1,18 @@
 console.log('Hola mundo');
+var nombre = 'Gabriela';
+var edad = 26;
+var peso = 157.5;
+var casado= false;
+var fechaNacimiento = new Date();
+var noExisto = null;
+var noEstoyDefinido = undefined;
+var a = -1;
+var b = 0;
+var c = 1;
+//cmd + a = seleccionar cod
+//cmd + alt +l = inventar cod
+if (a) {
+    console.log('Verdadero');
+}else {
+    console.log('Falso');
+}
