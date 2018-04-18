c898a17fae967df5320eae64fe6a15549501a52a
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-04-18 12:47:10 -0500

    4

--- Variables.js
+++ Variables.js
@@ -1,8 +1,13 @@
 console.log('Hola mundo');
+var mutar;
 var nombre = 'Gabriela';
+mutar = nombre;
 var edad = 26;
+mutar = edad;
 var peso = 157.5;
+mutar = peso;
 var casado= false;
+mutar = casado;
 var fechaNacimiento = new Date();
 var noExisto = null;
 var noEstoyDefinido = undefined;
@@ -16,3 +21,21 @@ if (a) {
 }else {
     console.log('Falso');
 }
+
+//objetos json
+var usuario = {
+  "nombre": "Gabriela",
+    apellido: "Cuascota",
+    cedula: '1728394675',
+    edad: 28,
+};
+console.log(usuario.nombre);
+console.log(usuario);
+delete usuario.edad; // borrar variables
+console.log(usuario);
+usuario.fechaNacimiento = new Date(); // crear cosas a 1 nivel
+console.log(usuario);
+usuario.mascotas = {};
+usuario.mascotas.nombre = 'gabby';//crear cosas a diferente nivel
+console.log(usuario);
+var arreglo = [1, "Gabriela", true, undefined, null, new Date(),{nombre: "Gabriela"}, [1, 2, 3, true]];
